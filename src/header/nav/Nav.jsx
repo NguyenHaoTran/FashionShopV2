85e8c06e8458b540a16db585758bd0f0ca813e1b
NguyenHao <Henryporison@outlook.com.vn>
2024-10-10 21:33:57 +0700

    new

--- src/header/nav/Nav.jsx
+++ src/header/nav/Nav.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
-import { useState } from "react";
+import React from 'react';
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./nav.scss"
+import "./nav.scss";
 
-const Nav = ({isOpen, toggleMenu}) => {
-   const handleMenuItemClick = () => {
-    // Đóng menu khi một mục được chọn
-    toggleMenu();
-   };
+const Nav = ({ isOpen, toggleMenu }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleMenuItemClick = () => {
+    // Chỉ đóng menu khi ở kích thước mobile
+    if (isMobile) {
+      toggleMenu();
+    }
+  };
 
   return (
     <nav className={`menu ${isOpen ? 'active' : ''}`}>
-        <ul>
-          <li onClick={handleMenuItemClick}><Link to ="/">TRANG CHỦ</Link></li>
-          <li onClick={handleMenuItemClick}><Link to="/about">VỀ CHÚNG TÔI</Link></li>
-          <li onClick={handleMenuItemClick}><Link to="/products">SẢN PHẨM</Link></li>
-          <li onClick={handleMenuItemClick}><Link to="/news">TIN TỨC</Link></li>
-          <li onClick={handleMenuItemClick}><Link to="/contacts">LIÊN HỆ</Link></li>        
-        </ul>
-      </nav>
-  )
-}
+      <ul>
+        <li onClick={handleMenuItemClick}><Link to="/">TRANG CHỦ</Link></li>
+        <li onClick={handleMenuItemClick}><Link to="/about">VỀ CHÚNG TÔI</Link></li>
+        <li onClick={handleMenuItemClick}><Link to="/products">SẢN PHẨM</Link></li>
+        <li onClick={handleMenuItemClick}><Link to="/news">TIN TỨC</Link></li>
+        <li onClick={handleMenuItemClick}><Link to="/contacts">LIÊN HỆ</Link></li>        
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
