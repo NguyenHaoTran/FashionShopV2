47106630a75ec67654a1f80bb07f93a7f3682419
NguyenHao <Henryporison@outlook.com.vn>
2024-10-19 22:31:43 +0700

    linenav

--- src/header/blackLine/BlackLine.jsx
+++ src/header/blackLine/BlackLine.jsx
@@ -1,6 +1,6 @@
 import "./blackLine.scss";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const BlackLine = () => {
   return (
@@ -9,7 +9,10 @@ const BlackLine = () => {
         <a href="#">Tài khoản</a>
       </div>
       <div className="blackLine_text">
-        <p>Chương trình giảm giá theo mùa đã bắt đầu! Giảm tới 50%! <Link to="/FashionShopV2/products">Mua ngay</Link></p>
+        <p>
+          Chương trình giảm giá theo mùa đã bắt đầu! Giảm tới 50%!{" "}
+          <Link to="/FashionShopV2/products">Mua ngay</Link>
+        </p>
       </div>
       <div className="language">
         <div className="items">
